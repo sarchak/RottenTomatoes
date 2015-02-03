3c7f2ab32d483370add380c08be8d3da9e5df804
Shrikar Archak <shrikar@viralheat.com>
2015-02-02 22:36:10 -0800

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Shrikar Archak on 2/2/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
